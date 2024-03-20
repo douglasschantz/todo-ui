fd2e9fc3380af72939fb2b0bd69c8f3bd56da430
Douglas Schantz <schantzdouglas@gmail.com>
2024-03-19 21:06:44 -0300

    DeleteTodo implement

--- src/components/ListTodoComponent.jsx
+++ src/components/ListTodoComponent.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { getAllTodos } from '../services/TodoService'
+import { deleteTodo, getAllTodos } from '../services/TodoService'
 import { useNavigate } from 'react-router-dom'
 
 const ListTodoComponent = () => {  
@@ -29,6 +29,14 @@ const ListTodoComponent = () => {
         navigate(`/update-todo/${id}`)
     }
 
+    function removeTodo(id){
+        deleteTodo(id).then((response)=>{
+            listTodos();
+        }).catch(error=>{
+            console.error(error);
+        })
+    }
+
     return (
         <div className='container'>
             <h2 className='text-center' >List of Todos</h2>
@@ -54,6 +62,7 @@ const ListTodoComponent = () => {
                                     <td>{todo.completed ? 'YES': 'NO'}</td>
                                     <td>
                                         <button className='btn btn-info' onClick={()=> updateTodo(todo.id)}>Update</button>
+                                        <button className='btn btn-danger' style={{marginLeft:"10px"}} onClick={() => removeTodo(todo.id)}>Delete</button>
                                     </td>
                                 </tr>
                             )
